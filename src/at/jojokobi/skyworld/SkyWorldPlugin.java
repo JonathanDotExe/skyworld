df100393b668b403378f6b3c3a54465168a477a0
DatDonathan <jojo08082003@gmail.com>
2021-02-15 11:02:56 +0100

    Worked further on new generator

--- src/at/jojokobi/skyworld/SkyWorldPlugin.java
+++ src/at/jojokobi/skyworld/SkyWorldPlugin.java
@@ -9,7 +9,6 @@ import java.io.IOException;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.WorldCreator;
-import org.bukkit.WorldType;
 import org.bukkit.plugin.java.JavaPlugin;
 import at.jojokobi.skyworld.generation.islands.*;
 import at.jojokobi.skyworld.generation.SkyWorldOverworldGenerator;
@@ -27,27 +26,16 @@ public class SkyWorldPlugin extends JavaPlugin{
 	private Island endIsland;
 	private Island enderPortalIsland;
 	private World skyWorld;
-	private World skyNether;
-	private World skyEnd;
-	private boolean exists = false;
-	private boolean netherExists = false;
-	private boolean endExists = false;
 	private PlayerSpawnSetter spawner;
+	private SkyWorldOverworldGenerator gen = new SkyWorldOverworldGenerator();
 	
 	@Override
 	public void onEnable(){
-		if (this.getServer().getWorld("skyworld") == null) {
-			exists = false;
-		}
-		else {
-			exists = true;
-		}
 		loadBools();
 		generate();
 		spawner = new PlayerSpawnSetter(skyWorld);
 		getServer().getPluginManager().registerEvents(spawner, this);
-		getServer().getPluginManager().registerEvents(new NetherSwitcher(skyWorld, skyNether, skyEnd), this);
-		getServer().getPluginManager().registerEvents(new SkyWorldOverworldGenerator(), this);
+		//getServer().getPluginManager().registerEvents(new NetherSwitcher(skyWorld, skyNether, skyEnd), this);
 		spawner.load();
 		islands = new Island[] {new ClassicIsland(true), new RainbowIsland(true),  new ForestIsland(true),  new HouseIsland(true), new FarmIsland(true), new LlamaIsland(true), new MineIsland(true), new MiniDungeonIsland(true)};
 		locations = new Location[] {new Location(skyWorld, 64, 64, 0), new Location(skyWorld, 32, 64, 32), new Location(skyWorld, 0, 64, 64), new Location(skyWorld, -32, 64, 32), new Location(skyWorld, -64, 64, 0), new Location(skyWorld, -32, 64, -32), new Location(skyWorld, 0, 64, -64), new Location(skyWorld, 32, 64, -32)};
@@ -72,12 +60,13 @@ public class SkyWorldPlugin extends JavaPlugin{
 	private void generate(){
 		//Overworld
 		WorldCreator generator = new WorldCreator("skyworld");
-		generator.type(WorldType.FLAT);
-		generator.generatorSettings("{\"structures\":{\"structures\":{}}, \"biome\":\"minecraft:ocean\", \"layers\":[{\"block\":\"minecraft:air\", \"height\":1}]}");
+		generator.generator(gen);
+		//generator.type(WorldType.FLAT);
+		//generator.generatorSettings("{\"structures\":{\"structures\":{}}, \"biome\":\"minecraft:ocean\", \"layers\":[{\"block\":\"minecraft:air\", \"height\":1}]}");
 		skyWorld = this.getServer().createWorld(generator);
 		skyWorld.setSpawnLocation(64, 70, 0);
 		//Nether
-		WorldCreator netherGenerator = new WorldCreator("skynether");
+		/*WorldCreator netherGenerator = new WorldCreator("skynether");
 		netherGenerator.type(WorldType.FLAT);
 		netherGenerator.generatorSettings("{\"structures\":{\"structures\":{}}, \"biome\":\"minecraft:nether\", \"layers\":[{\"block\":\"minecraft:air\", \"height\":1}]}");
 		skyNether = this.getServer().createWorld(netherGenerator);
@@ -87,22 +76,21 @@ public class SkyWorldPlugin extends JavaPlugin{
 		endGenerator.type(WorldType.FLAT);
 		endGenerator.generatorSettings("{\"structures\":{\"structures\":{}}, \"biome\":\"minecraft:the_end\", \"layers\":[{\"block\":\"minecraft:air\", \"height\":1}]}");
 		skyEnd = this.getServer().createWorld(endGenerator);
-		skyEnd.setSpawnLocation(0, 70, 0);
+		skyEnd.setSpawnLocation(0, 70, 0);*/
 	}
 	
 	private void generateSpawnIslands(){
-		if (!exists){
-			for (int i = 0; i < islands.length; i++){
-				try{
-					islands[i].build(locations[i]);
-				}
-				catch(Exception e){
-					e.printStackTrace();
-					System.out.println("[SkyWorld]: Generation Error!");
-				}
+		for (int i = 0; i < islands.length; i++){
+			try{
+				islands[i].build(locations[i]);
 			}
-			middleIsland.build(new Location(skyWorld, -1, 64, -1));
+			catch(Exception e){
+				e.printStackTrace();
+				System.out.println("[SkyWorld]: Generation Error!");
+			}
 		}
+		middleIsland.build(new Location(skyWorld, -1, 64, -1));
+		/*
 		if (!netherExists){
 			netherIsland.build(new Location(skyNether, -1, 64, -1));
 		}
@@ -110,53 +98,7 @@ public class SkyWorldPlugin extends JavaPlugin{
 		if (!endExists){
 			endIsland.build(new Location(skyEnd, -1, 64, -1));
 			enderPortalIsland.build(new Location(skyEnd, -4, 64, 64));
-		}
-	}
-	
-	private void loadBools(){
-		try {
-			BufferedReader fileNether = new BufferedReader (new FileReader("nether.txt"));
-			String text = fileNether.readLine();
-			if (text.equals("true")){
-				netherExists = true;
-			}
-			else{
-				netherExists = false;
-			}
-			fileNether.close();
-		} catch (IOException e) {
-			System.out.println("Error beim Laden von Nether!");
-		}
-		try {
-			BufferedReader fileEnd = new BufferedReader (new FileReader("end.txt"));
-			String text = fileEnd.readLine();
-			if (text.equals("true")){
-				endExists = true;
-			}
-			else{
-				endExists = false;
-			}
-			fileEnd.close();
-		} catch (IOException e) {
-			System.out.println("Error beim Laden von End!");
-		}
-	}
-	
-	private void saveBools(){
-		try {
-			BufferedWriter fileNether = new BufferedWriter (new FileWriter("nether.txt"));
-			fileNether.write("true");
-			fileNether.close();
-		} catch (IOException e) {
-			System.out.println("Error beim Speichern von Nether!");
-		}
-		try {
-			BufferedWriter fileEnd = new BufferedWriter (new FileWriter("end.txt"));
-			fileEnd.write("true");
-			fileEnd.close();
-		} catch (IOException e) {
-			System.out.println("Error beim Speichern von End!");
-		}
+		}*/
 	}
 	
 }
